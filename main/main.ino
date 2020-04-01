5919af2c8045d0fb7b02114780ce0bb60ebffa65
James <huttonjames123@gmail.com>
2020-04-01 09:13:50 -0400

    removed obsolete PCF and TPA libs

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,9 @@
 #include <SparkFunSi4703.h>
-//#include <tpa2016.h>
-#include <Wire.h>
-#include "PCF8575.h"
 
-#include "tpa2016.h"
+#include <Wire.h>
+
+
+
 
 #include <SPI.h>
 #include <SD.h>
@@ -14,6 +14,14 @@
 File root;
 
 
+//////////////////////////////////////////////////////////////////////////////
+#include <SparkFunSX1509.h> // Include SX1509 library
+
+// SX1509 I2C address (set by ADDR1 and ADDR0 (00 by default):
+const byte SX1509_ADDRESS = 0x3E;  // SX1509 I2C address
+SX1509 io; // Create an SX1509 object to be used throughout
+//////////////////////////////////////////////////////////////////////////////
+
 #include <Arduino.h>
 #ifdef ESP32
   #include <WiFi.h>
@@ -22,28 +30,6 @@ File root;
   #include <ESP8266WiFi.h>
 #endif
 
-const bool WiFiEnabled = false;
-
-
-  WiFiServer server(80);
-  // Variable to store the HTTP request
-  String header;
-  // Decode HTTP GET value
-  String valueString = String(5);
-  int pos1 = 0;
-  int pos2 = 0;
-
-//const char* ssid = "FBI Van 003";
-//const char* password = "qwertyuiop";
-
-//const char* ssid = "Hope-Bible";
-//const char* password = "Th@nk.You1000!";
-
-//const char* ssid = "Galcom Guests";
-//const char* password = "GoodGoodTea";
-
-const char* ssid = "Galcom Staff";
-const char* password = "RadioActiveLifeChangingMedia";
 
 
 #include "AudioFileSourceSD.h"
@@ -58,26 +44,25 @@ AudioOutputI2S *out;
 AudioFileSourceID3 *id3;
 
 
-
-#define pcf_addr B0100000
-#define tpa_addr B1011000
 #define fm_addr  0x10 
 
 int resetPin = D0;  //GPIO16 HOTWIRE
 int SDIO = D2;
 int SCLK = D1;
 
-tpa tpa;
+
 
 Si4703_Breakout radio(resetPin, SDIO, SCLK);
 int channel = 947;
 int volume = 5;
 char rdsBuffer[10];
 
-float mp3_gain = 0.5;
-int8_t amp_gain = 0;
+#define MP3_MAX_GAIN    1.0
+#define MAX_VOLUME      15    //used for both Radio and MP3 volume!
+float mp3_gain = 0.5;     //starting level
 
-PCF8575 pcf8575(pcf_addr,SDIO,SCLK);
+
+
 void displayInfo();
 
 String mp3_name[100];
@@ -246,146 +231,96 @@ void switch_mode_mp3()
   device_mode = MP3_MODE;
 }
 
-void init_server()
-{
-  // Connect to Wi-Fi network with SSID and password
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
- 
-  WiFi.begin(ssid, password);
-  int counter = 0;
-  bool fail_connect = false;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
-    Serial.print(".");
-    counter++;
-    if(counter>80) {  //timeout after 8 seconds
-      Serial.println();
-      Serial.println("could not connect...");
-      return;
-    }
-  }
+
+
+//LATCHES SUICIDE CIRCUIT BUY SETTING SX1509 OSCIO PIN HIGH
+void latchPower() 
+{ 
+   //latch power 
+  Wire.beginTransmission(SX1509_ADDRESS); 
+  Wire.write(0x1E);  
+  Wire.write(B01011111);         //set internal osc, set OSCIO to output, and HIGH    ///THIS KEEPS SUICIDE CIRCUIT ENABLED 
+  Wire.endTransmission(); 
+  Serial.println("power latched"); 
+} 
+
+//DE-LATCHES SUICIDE CIRCUIT BUY SETTING SX1509 OSCIO PIN LOW
+void powerDown() 
+{ 
+  Serial.println("turning off power..."); 
+       
+  io.digitalWrite(LED1, HIGH); io.pinMode(LED1, INPUT);  //this is only temporary for Proto3 board, cuz the LEDs are connected to VBAT with hotwire...
+  io.digitalWrite(LED2, HIGH); io.pinMode(LED2, INPUT);  
+  io.digitalWrite(LED3, HIGH); io.pinMode(LED3, INPUT);  
+  io.digitalWrite(LED4, HIGH); io.pinMode(LED4, INPUT);  
+
+  Wire.beginTransmission(SX1509_ADDRESS); 
+  Wire.write(0x1E);  
+  Wire.write(B01010000);         //set internal osc, set OSCIO to output, and LOW    ///THIS TURNS OF POWER TO ESP AND ALL PERIPHERALS
+  Wire.endTransmission();
   
-  Serial.println("");
-  Serial.println("WiFi connected");
- 
-  // Start the server
-  server.begin();
-  Serial.println("Server started");
- 
-  // Print the IP address
-  Serial.print("Use this URL to connect: ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
-}
+  delay(2000);  //wait for suicide circuit cap to empty 
+  Serial.println("this line should never be reached..."); 
+} 
 
-void server_tick()
+bool init_io()
 {
-  WiFiClient client = server.available();   // Listen for incoming clients
-
-  if (client) {                             // If a new client connects,
-    //Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        //Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial; margin-left:auto; margin-right:auto;}");
-            client.println(".slider { width: 300px; }</style>");
-            client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
-                     
-            // Web Page
-            client.println("</head><body><h1>RADIO VOLUME CONTROL</h1>");
-            client.println("<p>Volume: <span id=\"servoPos\"></span></p>");          
-            client.println("<input type=\"range\" min=\"0\" max=\"15\" class=\"slider\" id=\"servoSlider\" onchange=\"servo(this.value)\" value=\""+valueString+"\"/>");
-            
-            client.println("<script>var slider = document.getElementById(\"servoSlider\");");
-            client.println("var servoP = document.getElementById(\"servoPos\"); servoP.innerHTML = slider.value;");
-            client.println("slider.oninput = function() { slider.value = this.value; servoP.innerHTML = this.value; }");
-            client.println("$.ajaxSetup({timeout:1000}); function servo(pos) { ");
-            client.println("$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}</script>");
-           
-            client.println("</body></html>");     
-            
-            //GET /?value=180& HTTP/1.1
-            if(header.indexOf("GET /?value=")>=0) {
-              pos1 = header.indexOf('=');
-              pos2 = header.indexOf('&');
-              valueString = header.substring(pos1+1, pos2);
-              
-              //SET VOLUME
-              int val = valueString.toInt();
-              volume = val;
-              if (device_mode == MP3_MODE) { 
-                tpa.setGain(map(volume,0,15,-28,30));
-                Serial.println(volume);
-                updateLED();
-              } else if (device_mode == RADIO_MODE) {
-                radio.setVolume(volume);
-                Serial.println(volume);
-                updateLED();
-              }
-              
-              
-            }         
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
+  if (!io.begin(SX1509_ADDRESS))
+  {
+    
+    Serial.println("Failed to connect to SX1509...");
+    return 0;
   }
+
+  latchPower();
+
+  //init the rest of the IO 
+  io.pinMode(MUX_SEL, OUTPUT);  io.digitalWrite(MUX_SEL, LOW);    //*****This will set MUX_SEL high for 2ms and then low again. Therefore LPF in circuit is needed to remove, and stop FTDI from reseting ESP in a loop
+  io.pinMode(USB_SD_RST, OUTPUT); io.digitalWrite(USB_SD_RST, HIGH);    
+  io.pinMode(SW_MODE, INPUT); 
+  io.pinMode(SW_Q, INPUT); 
+  io.pinMode(LED1, OUTPUT); io.digitalWrite(LED1, LOW); 
+  io.pinMode(LED2, OUTPUT); io.digitalWrite(LED2, LOW);
+  io.pinMode(LED3, OUTPUT); io.digitalWrite(LED3, LOW);
+  io.pinMode(LED4, OUTPUT); io.digitalWrite(LED4, LOW);
+  io.pinMode(SW_VDOWN, INPUT); 
+  io.pinMode(SW_VUP, INPUT); 
+  io.pinMode(SW_DOWN, INPUT); 
+  io.pinMode(SW_UP, INPUT); 
+  io.pinMode(SW_RIGHT, INPUT); 
+  io.pinMode(SW_LEFT, INPUT); 
+  io.pinMode(SW_PLAY, INPUT);  
+  io.pinMode(SW_POW, INPUT); 
+
+
+
+  Serial.println("IO Initialized");
+  //wait for power to be released
+  while(io.digitalRead(SW_POW)) {delay(100);} //don't proceed until user releases power button
+
+  return 1; //success
+  
 }
-
-
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600); Serial.println(); Serial.println("boot"); 
+  delay(100);
+  // Set pinModes
+  init_io();                                                                      
 
-                                                                       
 
-  if (WiFiEnabled) {
-    init_server();
-  } else {
-    WiFi.mode(WIFI_OFF); 
-    WiFi.forceSleepBegin(); //<-- saves like 100mA!
-  }
+  WiFi.mode(WIFI_OFF); 
+  WiFi.forceSleepBegin(); //<-- saves like 100mA!
+
+  //RESET SD card 
+  io.digitalWrite(USB_SD_RST, LOW);
+  delay(100);
+  io.digitalWrite(USB_SD_RST, HIGH);
   
   SD.begin(D0);
+
+  
   listFolders();
   listFiles();
 
@@ -399,35 +334,21 @@ void setup()
     
   
   
-  // Set pinModes
   
-  pcf8575.pinMode(MUX_SEL, OUTPUT);       pcf8575.digitalWrite(MUX_SEL, LOW);
-  pcf8575.pinMode(AMP_SHUTOFF, OUTPUT);   pcf8575.digitalWrite(AMP_SHUTOFF, LOW);
-  pcf8575.pinMode(RADIO_SHUTOFF, OUTPUT); pcf8575.digitalWrite(RADIO_SHUTOFF, LOW);
-  pcf8575.pinMode(RADIO_RST, OUTPUT);   pcf8575.digitalWrite(RADIO_RST, LOW);
   
-  pcf8575.pinMode(LED1, OUTPUT);   pcf8575.digitalWrite(LED1, LOW);
-  pcf8575.pinMode(LED2, OUTPUT);   pcf8575.digitalWrite(LED2, LOW);
-  pcf8575.pinMode(LED3, OUTPUT);   pcf8575.digitalWrite(LED3, LOW);
-  pcf8575.pinMode(LED4, OUTPUT);   pcf8575.digitalWrite(LED4, LOW);
-  
-  pcf8575.begin();
 
 
 
-  //SET UP TPA AMP
-  tpa.setGain(tpa.gain);
-  
+  updateLED();
 }
 
 int ms = 0;
 int last_ms = 0;
-byte pcf_byte[2];
-byte last_pcf_byte[2];
+
 void loop()
 {while(1){
 
-  delayMicroseconds(10);
+  //delayMicroseconds(10);
 
   if (mp3_play){
     if (mp3->isRunning()) {
@@ -443,29 +364,17 @@ void loop()
   if (ms == last_ms) continue;
   last_ms = ms;
 
-  
-  if (!(ms%20)){
-
-
-    if (WiFiEnabled) server_tick();
-    
-  }
 
   if (!(ms%50)){
 
-                                                         
-    /*    
-    Wire.requestFrom(pcf_addr,2);
+                                                
     
-    pcf_byte[0] = Wire.read(); pcf_byte[1] = Wire.read();
-    
-    
-    if (pcf_byte[1] != last_pcf_byte[1]) {
-        if(pcf_byte[1] & SW_VUP_MASK){
+    if (1) {
+        if(io.digitalRead(SW_VUP)){
           if (device_mode == MP3_MODE) {
             volume ++;
-            if (volume == 16) volume = 15;
-            mp3_gain = ((volume*volume*volume)/1000.0)+(volume/30.0);
+            if (volume > MAX_VOLUME) volume = MAX_VOLUME;
+            mp3_gain = mapf(volume, 0, MAX_VOLUME, 0, MP3_MAX_GAIN);
             out->SetGain(mp3_gain);
             Serial.print(volume); Serial.print(" ("); Serial.print(mp3_gain); Serial.println(")");
             updateLED();
@@ -479,14 +388,13 @@ void loop()
           
           
         }
-        if(pcf_byte[1] & SW_VDOWN_MASK){
+        if(io.digitalRead(SW_VDOWN)){
           if (device_mode == MP3_MODE) {
             volume --;
             if (volume < 0) volume = 0;     
-            mp3_gain = ((volume*volume*volume)/1000.0)+(volume/30.0);
+            mp3_gain = mapf(volume, 0, MAX_VOLUME, 0, MP3_MAX_GAIN);
             out->SetGain(mp3_gain);
             Serial.print(volume); Serial.print(" ("); Serial.print(mp3_gain); Serial.println(")");
-            updateLED();
             updateLED();
           } else if (device_mode == RADIO_MODE) {
             volume --;
@@ -497,7 +405,7 @@ void loop()
           }
           
         }
-        if(pcf_byte[1] & SW_RIGHT_MASK){
+        if(io.digitalRead(SW_RIGHT)){
           if (device_mode == MP3_MODE) {
             mp3_index++;
             if (mp3_index >= (mp3_count)) mp3_index = 0;  //loop back to 0 after last song
@@ -510,7 +418,7 @@ void loop()
           }
           
         }
-        if(pcf_byte[1] & SW_LEFT_MASK){
+        if(io.digitalRead(SW_LEFT)){
           if (device_mode == MP3_MODE) {
             mp3_index--;
             if (mp3_index < 0) mp3_index = mp3_count-1;  //loop to last song
@@ -523,7 +431,7 @@ void loop()
           }
           
         }
-        if(pcf_byte[1] & SW_UP_MASK){
+        if(io.digitalRead(SW_UP)){
           if (device_mode == MP3_MODE) {
             folder_index --;
             if (folder_index < 0) folder_index = folder_count-1;  //loop back to 0 after last folder
@@ -537,7 +445,7 @@ void loop()
           }
           
         }
-        if(pcf_byte[1] & SW_DOWN_MASK){
+        if(io.digitalRead(SW_DOWN)){
           if (device_mode == MP3_MODE) {
             folder_index ++;
             if (folder_index >= (folder_count)) folder_index = 0;  //loop back to 0 after last folder
@@ -550,14 +458,14 @@ void loop()
             updateLED();
           }
         }
-        if(pcf_byte[1] & SW_POW_MASK){
+        if(io.digitalRead(SW_MODE)){
           if (device_mode == MP3_MODE) {
             switch_mode_radio();
           } else if (device_mode == RADIO_MODE) {
             switch_mode_mp3();
           } 
         }
-        if(pcf_byte[1] & SW_PLAY_MASK){
+        if(io.digitalRead(SW_PLAY)){
           if (device_mode == MP3_MODE) {
             if (mp3_play){
               mp3_play = false;
@@ -572,30 +480,72 @@ void loop()
           }
           
         }
-    }
-    last_pcf_byte[1] = pcf_byte[1];
+
+        if(io.digitalRead(SW_Q)){
+          readSD();
+        }
+
+        if(io.digitalRead(SW_POW)){
+          powerDown();
+        }
+        
     
     
     
     
     delay(1); //removing this makes the watchdog trip in radio mode....idk...reduce?  
-  }  
+  }}  
 }}
+
+//never leaves this function
+void readSD()
+{
+  while(!io.digitalRead(SW_Q))  delay(100); //wait for switch to be released before proceeding
+  
+  Serial.println("switching to SD card...");
+
+  //switch ESP SD pins to high impedance
+  pinMode(D0, INPUT);
+  pinMode(D5, INPUT);
+  pinMode(D6, INPUT);
+  pinMode(D7, INPUT);
+
+  //orient USB MUX from serial to SD
+  io.digitalWrite(MUX_SEL, HIGH);
+  //RESET SD card and SD reader
+  io.digitalWrite(USB_SD_RST, LOW);
+  delay(100);
+  io.digitalWrite(USB_SD_RST, HIGH);
+
+  //loop until SW_POW is pressed (should reset ESP though when MUX_SEL goes low and triggers FTDI...)
+  while(1){
+    delay(50);
+    if (io.digitalRead(SW_Q)){
+      io.digitalWrite(MUX_SEL, LOW);    //this will reset the ESP...
+      //RESET SD card and SD reader
+      io.digitalWrite(USB_SD_RST, LOW);   //not reached...
+      delay(100);
+      io.digitalWrite(USB_SD_RST, HIGH);
+      return;
+    }
+  }
+}
+
 
 //remember, LEDs are reversed (0 = on)
 void updateLED(){
-  pcf8575.digitalWrite(LED1, 1);pcf8575.digitalWrite(LED2, 1);pcf8575.digitalWrite(LED3, 1);pcf8575.digitalWrite(LED4, 1);
+  io.digitalWrite(LED1, 1);io.digitalWrite(LED2, 1);io.digitalWrite(LED3, 1);io.digitalWrite(LED4, 1);
   if (volume>=1) {
-    pcf8575.digitalWrite(LED1, 0);
+    io.digitalWrite(LED1, 0);
   }
   if (volume>=4) {
-    pcf8575.digitalWrite(LED2, 0);
+    io.digitalWrite(LED2, 0);
   }
   if (volume>=8) {
-    pcf8575.digitalWrite(LED3, 0);
+    io.digitalWrite(LED3, 0);
   }
   if (volume>=12) {
-    pcf8575.digitalWrite(LED4, 0);
+    io.digitalWrite(LED4, 0);
   }
 }
 
@@ -603,4 +553,10 @@ void displayInfo()
 {
    Serial.print("Channel:"); Serial.print(channel); 
    Serial.print(" Volume:"); Serial.println(volume); 
+}
+
+
+float mapf(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
